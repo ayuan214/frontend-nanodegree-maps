6945e69bdc66b46db6aedb52ffe1b728f3ec648b
ayuan214 <alvinyuan@gmail.com>
2015-05-07 14:02:56 -0700

    Full Screen Map
    - Included Google Maps Javascript v3 API Key
    - Allowed map to fit full screen on the browser window

--- js/app.js
+++ js/app.js
@@ -1,35 +1,16 @@
-ko.bindingHandlers.googlemap = {
-    init: function (element, valueAccessor) {
-        var
-        	value = valueAccessor(),
-        	mapOptions = {
-        		zoom: 10,
-        		center: new google.maps.LatLng(value.centerLat, value.centerLon),
-        		mapTypeId: google.maps.MapTypeId.ROADMAP
-            },
+var ViewModel = function() {
+    // initalize map
 
-        map = new google.maps.Map(element, mapOptions);
-          
-        for (var l in value.locations())
-        {
-            var latLng = new google.maps.LatLng(
-                value.locations()[l].latitude, 
-                value.locations()[l].longitude
-            );
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-            });
-        }
-    }
+    initializeMap();
+    var map;
+
+    function initializeMap(){
+        var mapOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(37.778790, -122.389259)
+        };
+        var map = new google.maps.Map(document.getElementById("map_container"), mapOptions);
+    } 
 };
-
-var vm =  {
-    locations: ko.observableArray([
-    	{name: "Cleveland", latitude:41.48 , longitude:-81.67},
-        {name: "Parma", latitude: 41.40, longitude: -81.73}
-    ])
-}
     
-ko.applyBindings(vm);
-
+ko.applyBindings(ViewModel());
