61d23b3312436a01c10684b5b7902ca9db044fdf
ayuan214 <alvinyuan@gmail.com>
2015-05-13 10:34:29 -0700

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # frontend-nanodegree-maps
 Google Maps with AJAX and Knockout.js
+
+Instructions:
+
+Make sure you have an internet connection
+Open index.html
+Click on the markers to open up the infoWindow to see the Yelp Business information
+Type into the text field to filter the markers.
+You can also click on the place on the list to open the info window
--- js/app.js
+++ js/app.js
@@ -1,1 +1,181 @@
-function Places(a){this.title=a.title,this.LatLng=a.gLatLng,this.bizID=a.bizID,this.marker=a.marker}function initializeMap(){var a={zoom:12,center:new google.maps.LatLng(37.77879,-122.389259)};return new google.maps.Map(document.getElementById("map_container"),a)}function initializeMarkers(a,b,c){for(var d=[],e=new google.maps.LatLngBounds,f=0;f<a.length;f++){var g=a[f].title,h=a[f].gLatLng[0],i=a[f].gLatLng[1],j=new google.maps.LatLng(h,i);d[f]=new google.maps.Marker({position:j,title:g}),a[f].marker=d[f],e.extend(j),google.maps.event.addListener(a[f].marker,"click",function(d){return function(){yelpAPI(b,a[d].marker,a[d].title,Model[d].bizID,c)}}(f))}return b.fitBounds(e),c}function yelpAPI(a,b,c,d,e){var f={consumerKey:"gRRJAGvD01BXyeZkXt9kUw",consumerSecret:"RqcoTBNLQaRAbV1bPtFqS_vdWA8",accessToken:"JbLZZ9MPRsM7tlv6WXwVex6KfU1t9p8z",accessTokenSecret:"8fasPNOG1EXM8dNQQx5n96VHVqk",serviceProvider:{signatureMethod:"HMAC-SHA1"}},g=d,h={consumerSecret:f.consumerSecret,tokenSecret:f.accessTokenSecret};parameters=[],parameters.push(["callback","cb"]),parameters.push(["oauth_consumer_key",f.consumerKey]),parameters.push(["oauth_consumer_secret",f.consumerSecret]),parameters.push(["oauth_token",f.accessToken]),parameters.push(["oauth_signature_method","HMAC-SHA1"]);var i={action:"http://api.yelp.com/v2/business/"+g,method:"GET",parameters:parameters};OAuth.setTimestampAndNonce(i),OAuth.SignatureMethod.sign(i,h);var j=OAuth.getParameterMap(i.parameters);console.log(j),$.ajax({url:i.action,data:j,cache:!0,dataType:"jsonp",jsonpCallback:"cb",success:function(d){console.log(d),console.log(d.rating_img_url);var h;h="<div id = 'content'><h3>"+c+"</h3>"+"<img class = 'yelp_image' src ="+d.rating_img_url+" alt = 'Yelp Review'></div>",e.setContent(h),e.open(a,b)},error:function(){var d;d="<div id = 'content'><h3>"+c+"</h3>"+"<p>Yelp Review unavailable</p></div>",e.setContent(d),e.open(a,b)}})}var Model=[{title:"AT&T Park",gLatLng:[37.778582,-122.38927],bizID:"at-and-t-park-san-francisco"},{title:"Ghirardelli Square",gLatLng:[37.805832,-122.423011],bizID:"ghirardelli-square-san-francisco?osq=Ghirdelli"},{title:"Oracle Arena",gLatLng:[37.75035,-122.203031],bizID:"oracle-arena-oakland"},{title:"Cliff House",gLatLng:[37.778472,-122.513952],bizID:"the-cliff-house-bistro-san-francisco"},{title:"Coit Tower",gLatLng:[37.802392,-122.405821],bizID:"coit-tower-san-francisco"},{title:"Orpheum Theatre",gLatLng:[37.779383,-122.414713],bizID:"orpheum-theatre-san-francisco"},{title:"Ike's Place",gLatLng:[37.764202,-122.430606],bizID:"ikes-place-san-francisco"}],ViewModel=function(){var a=this;a.filter=ko.observable(""),a.placesList=ko.observableArray([]);var b=initializeMap();infowindows=new google.maps.InfoWindow,a.map=ko.observable(b),a.infowindows=ko.observable(infowindows),initializeMarkers(Model,b,infowindows),Model.forEach(function(b){a.placesList.push(new Places(b))}),a.filteredItems=ko.computed(function(){return ko.utils.arrayFilter(a.placesList(),function(c){return-1!==c.title.toLowerCase().indexOf(a.filter().toLowerCase())?c.marker.setMap(b):c.marker.setMap(null),-1!==c.title.toLowerCase().indexOf(a.filter().toLowerCase())})},a),a.clickList=function(b){yelpAPI(a.map(),b.marker,b.title,b.bizID,a.infowindows())}};ko.applyBindings(new ViewModel);
+// API to use are Google and Yelp
+
+/* *********************************** Model ***************************************/
+
+var Model = [
+    {
+        title: 'AT&T Park', // name of place
+        gLatLng: [37.778582, -122.389270], //latlng of place for gmaps
+        bizID: 'at-and-t-park-san-francisco' // bizID to do yelp search
+    },
+    {
+        title: 'Ghirardelli Square',
+        gLatLng: [37.805832, -122.423011],
+        bizID: 'ghirardelli-square-san-francisco?osq=Ghirdelli'
+    },
+    {
+        title: 'Oracle Arena',
+        gLatLng: [37.750350, -122.203031],
+        bizID: 'oracle-arena-oakland'
+    },
+    {
+        title: 'Cliff House',
+        gLatLng: [37.778472, -122.513952],
+        bizID: 'the-cliff-house-bistro-san-francisco'
+    },
+    { 
+        title: 'Coit Tower',
+        gLatLng: [37.802392, -122.405821],
+        bizID: 'coit-tower-san-francisco'
+
+    },
+    {
+        title: 'Orpheum Theatre',
+        gLatLng: [37.779383, -122.414713],
+        bizID: 'orpheum-theatre-san-francisco'
+    },
+    {
+        title: 'Ike\'s Place',
+        gLatLng: [37.764202, -122.430606],
+        bizID: 'ikes-place-san-francisco'
+    }
+];
+// *********************************** View Model *********************************
+var ViewModel = function() {
+    var self = this;
+    self.filter = ko.observable(""); // create filter variable for textField to filter
+    self.placesList = ko.observableArray([]); // create placeList observable 
+
+    var map = initializeMap(); //create map on screen
+    infowindows = new google.maps.InfoWindow(); // create infowindows for gmaps
+    self.map = ko.observable(map); //make map observable
+    self.infowindows = ko.observable(infowindows); // make infowindows ko.observable
+    initializeMarkers(Model, map, infowindows); //create all markers on the map
+
+    Model.forEach(function(placeItem){ 
+        self.placesList.push( new Places(placeItem) );// fill placesList with Model data;
+    });
+
+    self.filteredItems = ko.computed(function() {
+        return ko.utils.arrayFilter(self.placesList(), function(places){
+            if (places.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1){
+                places.marker.setMap(map); //if there are matches make map on screen
+            }
+            else {
+                places.marker.setMap(null); //otherwise if no index map found set marker map to null
+            }
+            return places.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1; //filters the array
+        })
+    }, self);
+
+   self.clickList = function(data) {
+        //runs yelp function that will open info window
+    yelpAPI(self.map(), data.marker, data.title, data.bizID, self.infowindows());
+   }
+};
+
+// ******************************** End of View Model ******************************
+function Places(Model) {
+    this.title = Model.title;
+    this.LatLng = Model.gLatLng;
+    this.bizID = Model.bizID;
+    this.marker = Model.marker;
+}
+
+function initializeMap(){
+    var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(37.778790, -122.389259) // random center location chosen
+    };
+    return new google.maps.Map(document.getElementById("map_container"), mapOptions);
+}
+
+  // create function for markers
+function initializeMarkers(data, map_view, infowindows) {
+    var markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i =0; i<data.length; i++) {
+        var name = data[i].title;
+        var lat = data[i].gLatLng[0];
+        var lng = data[i].gLatLng[1];
+        var marker_latlng = new google.maps.LatLng(lat, lng); 
+
+        markers[i] = new google.maps.Marker({
+            position: marker_latlng,
+            title: name 
+        }); 
+        data[i].marker = markers[i];
+        bounds.extend(marker_latlng);
+
+        // need to use closures to make it so that it doesn't always reference last clicked index
+        google.maps.event.addListener(data[i].marker, 'click', function(j) {
+            return function() {
+                yelpAPI(map_view, data[j].marker, data[j].title, Model[j].bizID, infowindows); 
+            }
+        }(i));
+    }
+    map_view.fitBounds(bounds);
+    return data, markers, infowindows;
+} 
+
+function yelpAPI (map, markers, title, bizID, infowindows) {
+    var auth = {
+        consumerKey : "gRRJAGvD01BXyeZkXt9kUw",
+        consumerSecret : "RqcoTBNLQaRAbV1bPtFqS_vdWA8",
+        accessToken : "JbLZZ9MPRsM7tlv6WXwVex6KfU1t9p8z",
+        accessTokenSecret : "8fasPNOG1EXM8dNQQx5n96VHVqk",
+        serviceProvider : {
+            signatureMethod : "HMAC-SHA1"
+        }
+    };
+
+    var business = bizID;
+    var accessor = {
+        consumerSecret : auth.consumerSecret,
+        tokenSecret : auth.accessTokenSecret
+    };
+    parameters = [];
+    parameters.push(['callback', 'cb']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+    var message = {
+        'action' : 'http://api.yelp.com/v2/business/' + business,
+        'method' : 'GET',
+        'parameters' : parameters
+    };
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
+
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    console.log(parameterMap);
+
+    $.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'cache' : true, //this has to be included after jquery1.6
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'cb',
+        success : function(data, textStats, XMLHttpRequest) {
+            console.log(data);
+            console.log(data.rating_img_url);
+            var content;
+            content = '<div id = \'content\'><h3>' + title + '</h3>' + '<img class = \'yelp_image\' src =' + data.rating_img_url + ' alt = \'Yelp Review\'></div>' ;
+            infowindows.setContent(content); //fills content with whatever is clicked
+            infowindows.open(map, markers);  
+        },
+        error : function() {
+            var content;
+            content = '<div id = \'content\'><h3>' + title + '</h3>' + '<p>Yelp Review unavailable</p></div>' ;
+            infowindows.setContent(content); //fills content with whatever is clicked
+            infowindows.open(map, markers);  
+        }
+
+    });
+}
+
+// Initalize Function
+ko.applyBindings(new ViewModel());
